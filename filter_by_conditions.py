f61f8df3610dd153b1860b2e256e7ee79f240c8d
TheBookwyrms <60119564+TheBookwyrms@users.noreply.github.com>
2025-08-02 01:09:15 +0200

    ok there's a problem somewhere, will fix later

--- filter_by_conditions.py
+++ filter_by_conditions.py
@@ -1,8 +1,19 @@
 def filter_by_conditions(possible_schedules):
     filtered_sch = []
     for schedule in possible_schedules:
+        eth = False
+        eng = False
         for section in schedule.sections:
             if section.course_title == "Applied Ethics in Humanities":
                 if section.section_title == "Science Fiction and Ethics":
-                    filtered_sch.append(schedule)
+                    eth = True
+
+        for section in schedule.sections:
+            if section.course_title == "Literary Themes":
+                if section.section_title == "Time Travel in Literature":
+                    eng = True
+
+
+        if eng:
+            filtered_sch.append(schedule)
     return filtered_sch
--- use_schedule_maker.py
+++ use_schedule_maker.py
@@ -42,9 +42,9 @@ unique_sch_with_gym = get_unique_schedules(possible_sch_with_gym)
 #print(f'{unique_sch_with_gym=}'.split('=')[0])
 
 export_allowed_courses(allowed_sections)
-save_schedule_figs([mandatory_sections], folder_name=f'{mandatory_sections=}'.split('=')[0], show_var=False)
-save_schedule_figs(filt_poss_no_gym, folder_name=f'{filt_poss_no_gym=}'.split('=')[0], show_var=False)
-save_schedule_figs(filt_poss_with_gym, folder_name=f'{filt_poss_with_gym=}'.split('=')[0], show_var=True)
+#save_schedule_figs([mandatory_sections], folder_name=f'{mandatory_sections=}'.split('=')[0], show_var=False)
+#save_schedule_figs(filt_poss_no_gym, folder_name=f'{filt_poss_no_gym=}'.split('=')[0], show_var=False)
+#save_schedule_figs(filt_poss_with_gym, folder_name=f'{filt_poss_with_gym=}'.split('=')[0], show_var=True)
 #save_schedule_figs(unique_sch_no_gym, folder_name=f'{unique_sch_no_gym=}'.split('=')[0], show_var=True)
 #save_schedule_figs(unique_sch_with_gym, folder_name=f'{unique_sch_with_gym=}'.split('=')[0], show_var=True)
 
